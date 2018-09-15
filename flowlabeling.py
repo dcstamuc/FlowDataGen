d15ee06c7a560ba665acc6eda91e42bd7d710995
asim <asim@lbl.gov>
2018-09-15 15:58:22 -0700

    updated by CS

--- flowlabeling.py
+++ flowlabeling.py
@@ -1,6 +1,13 @@
+#
+# Flow Data Generation Copyright (c) 2018, All rights reserved.
+# 
+# If you have questions about your rights to use or distribute this
+# software, please contact dcs.tamuc@gmail.com
+# 
+# Fri Sep 14 07:37:35 CDT 2018
+# dcs.tamuc@gmail.com
+#
 # Tool to combine the flow and classifier
-# Caitlin Sim <caitlinsim@gmail.com>
-# Sep 15, 2018
 #
 '''
 python flowlabelling.py -h
--- flowsplitter.py
+++ flowsplitter.py
@@ -1,6 +1,13 @@
+#
+# Flow Data Generation Copyright (c) 2018, All rights reserved.
+# 
+# If you have questions about your rights to use or distribute this
+# software, please contact dcs.tamuc@gmail.com
+# 
+# Fri Sep 14 07:37:35 CDT 2018
+# dcs.tamuc@gmail.com
+#
 # Tool to split the flow files in timed order (e.g. 5 second)
-# Caitlin Sim <caitlinsim@gmail.com>
-# Aug 16, 2018
 #
 '''
 python flowsplitter.py -h
